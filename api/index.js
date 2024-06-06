038b6a97c9b36fa4cd2314f538a00ff370e72720
Gonzalo <gonzaloostickarr@gmail.com>
2024-06-06 14:15:53 -0300

    first commit

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'somevalue',
   resave: false,
   saveUninitialized: true,
   cookie: { secure: true }
